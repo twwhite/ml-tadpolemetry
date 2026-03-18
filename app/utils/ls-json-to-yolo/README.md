5d60be1136a60a6aed8e1cfb6699491f8f6241a1
Tim White <tim@bsi-cr.com>
2026-03-18 11:09:13 -0700

    Restructure

--- app/utils/ls-json-to-yolo/README.md
+++
@@ -1,8 +0,0 @@
-As far as I can tell, Label Studio does not presently have a tool to export keypoints with bounding boxes to the YOLO format.
-
-So, convert.py takes the standard format Label Studio .json output file, parses out the relevant bounding box and keypoint data, and exports the data to an output directory of your choice where each file will be a YOLO coordinate file for a single image.
-
-Right now, the convert.py file has the keypoint labels hard-coded, so you will need to change them when you are switching models.
-
-TODO: 
-- Auto-evaluate fields with a mapping file (no hard-coding)
---
+++ tadpolemetry/train.py
@@ -0,0 +1,25 @@
+import os
+from datetime import datetime
+
+import torch
+from ultralytics import YOLO
+
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
+
+model = YOLO("yolo26n.pt")
+cwd = os.getcwd()
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+model_name = "scale"
+model.train(
+    data=f"data/training/configs/config_{model_name}_model.yml",
+    epochs=100,
+    batch=32,
+    imgsz=640,
+    augment=True,
+    project=f"{cwd}/tadpolemetry/models/{model_name}_model_output",
+    name=f"run_{timestamp}",
+)
+
+# model.train(data='configs/config_spline_model.yaml', epochs=100, batch=32, imgsz=640, augment=True)
